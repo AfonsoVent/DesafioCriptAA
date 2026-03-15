110caf2194db22f270532e368519b4a029c94fd2
Afonso <l55958@alunos.uevora.pt>
2026-03-15 22:12:05 +0000

    SegundoDesafio

--- hjUDPproxy/hjUDPproxy.java
+++ hjUDPproxy/hjUDPproxy.java
@@ -21,12 +21,14 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
+import java.security.SecureRandom;
 import java.util.Arrays;
 import java.util.Properties;
 import java.util.Set;
 import java.util.stream.Collectors;
 import javax.crypto.Cipher;
-import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.ChaCha20ParameterSpec;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 class hjUDPproxy {
@@ -42,9 +44,9 @@ class hjUDPproxy {
         Properties properties = new Properties();
         properties.load(inputStream);
 	    
-        // Define AES key (the same one)
-        byte[] keyBytes = "0123456789abcdef".getBytes();
-        SecretKeySpec key = new SecretKeySpec(keyBytes, "AES");
+        // Chacha key
+        byte[] keyBytes = "0123456789abcdef0123456789abcdef".getBytes();
+        SecretKeySpec key = new SecretKeySpec(keyBytes, "ChaCha20");
 
         // Read endpoints (remote of prop and localdelivery of prop)
         String remote = properties.getProperty("remote");
@@ -60,6 +62,7 @@ class hjUDPproxy {
         
         // Buffer
         byte[] buffer = new byte[4 * 1024];
+        SecureRandom random = new SecureRandom();
        
         while (true) {
             // Prepare packet
@@ -68,6 +71,7 @@ class hjUDPproxy {
 
             // Debug "."
             System.out.print(".");
+
             for(SocketAddress outSocketAddress : outSocketAddressSet) 
             {   
                 // The Data
@@ -78,11 +82,11 @@ class hjUDPproxy {
                 byte[] cipherTxt = Arrays.copyOfRange(data, 12, data.length);
 
                 // Prepare Key
-                Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
-                GCMParameterSpec spec = new GCMParameterSpec(128, iv);
+                Cipher cipher = Cipher.getInstance("ChaCha20-Poly1305");
+                IvParameterSpec spec = new IvParameterSpec(iv);
                 cipher.init(Cipher.DECRYPT_MODE, key, spec);
 
-                // ""Cypher"" to plain
+                // Decrypt to plain
                 byte[] plain = cipher.doFinal(cipherTxt);
 
                 // Sends to player
