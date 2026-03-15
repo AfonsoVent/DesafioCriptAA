2b9ad9cf37ab923e5db8785f8d7899e475f4adc1
Afonso <l55958@alunos.uevora.pt>
2026-03-15 21:42:16 +0000

    1 Desafio

--- hjStreamServer/hjStreamServer.java
+++ hjStreamServer/hjStreamServer.java
@@ -3,82 +3,100 @@
 * Streaming server: streams video frames in UDP packets
 * for clients to play in real time the transmitted movies
 */
-// Test
 
 import java.io.*;
 import java.net.*;
-
+import javax.crypto.Cipher;
+import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.SecureRandom;
 
 class hjStreamServer {
-
 	static public void main( String []args ) throws Exception {
-	        if (args.length != 3)
-	        {
-                   System.out.println("Erro, usar: mySend <movie> <ip-multicast-address> <port>");
-	           System.out.println("        or: mySend <movie> <ip-unicast-address> <port>");
-	           System.exit(-1);
-                }
-      
-		int size;
-		int csize = 0;
-		int count = 0;
- 		long time;
+		// Valid the input of user
+	    if (args.length != 3)
+	    {
+            System.out.println("Erro, usar: mySend <movie> <ip-multicast-address> <port>");
+	       	System.out.println("        or: mySend <movie> <ip-unicast-address> <port>");
+	       	System.exit(-1);
+        }
+		
+		// Auxiliar Var
+		int size;	// of each frame
+		int csize = 0;	// size sent
+		int count = 0;	// counter frames
+ 		long time;	// timer
+
+		// Opens the movie
 		DataInputStream g = new DataInputStream( new FileInputStream(args[0]) );
 		byte[] buff = new byte[4096];
 
+		// Send socket UDP
 		DatagramSocket s = new DatagramSocket();
 		InetSocketAddress addr = new InetSocketAddress( args[1], Integer.parseInt(args[2]));
+		
+		// Create key
+		byte[] keyBytes = "0123456789abcdef".getBytes();
+		SecretKeySpec key = new SecretKeySpec(keyBytes, "AES");
+		SecureRandom random = new SecureRandom();	// SecureRandom() >> Random()
+
+		// Packet UDP sent  
 		DatagramPacket p = new DatagramPacket(buff, buff.length, addr );
+
 		long t0 = System.nanoTime(); // Ref. time 
 		long q0 = 0;
 
-
-	        // Movies are encoded in .dat files, where each
-          	// frame is encoded in a real-time sequence of MP4 frames 
-         	// Somewhat an FFMPEG4 playing scheme .. Dont worry
-		
-		// Each frame has:
-		// Short size || Long Timestamp || byte[] EncodedMP4Frame
-		// You can read (frame by frame to transmit ...
-		// But you must folow the "real-time" encoding conditions
-
-		// OK let's do it !
-
-		while ( g.available() > 0 ) {
-		    
+		// While there is movie to stream, them send...
+		while (g.available() > 0) {
 		    size = g.readShort(); // size of the frame
 		    csize=csize+size;
 		    time = g.readLong();  // timestamp of the frame
-			if ( count == 0 ) q0 = time; // ref. time in the stream
+			if (count == 0) q0 = time; // ref. time in the stream
 			count += 1;
-			g.readFully(buff, 0, size );
-			p.setData(buff, 0, size );
-			p.setSocketAddress( addr );
+			
+			// Read the bytes
+			g.readFully(buff, 0, size);
+			
+			// IV frame
+			byte[] iv = new byte[12];
+			random.nextBytes(iv);
 
+			// Starts AES-GCM
+			Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
+			GCMParameterSpec spec = new GCMParameterSpec(128, iv);
+			cipher.init(Cipher.ENCRYPT_MODE, key, spec);
+
+			// Cypher frame
+			byte[] cipherText = cipher.doFinal(buff, 0, size);
+
+			// Prepare array - Send: [IV, ciphertext]
+			byte[] packetData = new byte[iv.length + cipherText.length];
+			System.arraycopy(iv, 0, packetData, 0, iv.length);   
+			System.arraycopy(cipherText, 0, packetData, iv.length, cipherText.length);
+
+			// Data
+			p.setData(packetData, 0, packetData.length);
+			p.setSocketAddress(addr);
+
+			// Timer to streaming
 			long t = System.nanoTime(); // what time is it?
-
-			// Decision about the right time to transmit
 			Thread.sleep( Math.max(0, ((time-q0)-(t-t0))/1000000));
 		   
-		        // send datagram (udp packet) w/ payload frame)
-			// Frames sent in clear (no encryption)
-
+			// Frames sent encrypted using AES-GCM
 			s.send(p); 
 
-	           	// Just for awareness ... (debug)
-
-			System.out.print( ":" );
+	        // Debug "."
+			System.out.print( "." );
 		}
 
 		long tend = System.nanoTime(); // "The end" time 
-                System.out.println();
+        System.out.println();
 		System.out.println("DONE! all frames sent: "+ count);
 
 		long duration=(tend-t0)/1000000000;
 		System.out.println("Movie duration "+ duration + " s");
 		System.out.println("Throughput "+ count/duration + " fps");
-     	        System.out.println("Throughput "+ (8*(csize)/duration)/1000 + " Kbps");
-
+     	System.out.println("Throughput "+ (8*(csize)/duration)/1000 + " Kbps");
 	}
 }
 
--- hjUDPproxy/hjUDPproxy.java
+++ hjUDPproxy/hjUDPproxy.java
@@ -19,46 +19,81 @@ import java.io.FileInputStream;
 import java.io.InputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.MulticastSocket;
 import java.net.InetSocketAddress;
-import java.net.InetAddress;
 import java.net.SocketAddress;
 import java.util.Arrays;
 import java.util.Properties;
 import java.util.Set;
 import java.util.stream.Collectors;
+import javax.crypto.Cipher;
+import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 
 class hjUDPproxy {
     public static void main(String[] args) throws Exception {
+        // Open props
         InputStream inputStream = new FileInputStream("config.properties");
         if (inputStream == null) {
             System.err.println("Configuration file not found!");
             System.exit(1);
         }
+
+        // load them
         Properties properties = new Properties();
         properties.load(inputStream);
-	String remote = properties.getProperty("remote");
+	    
+        // Define AES key (the same one)
+        byte[] keyBytes = "0123456789abcdef".getBytes();
+        SecretKeySpec key = new SecretKeySpec(keyBytes, "AES");
+
+        // Read endpoints (remote of prop and localdelivery of prop)
+        String remote = properties.getProperty("remote");
         String destinations = properties.getProperty("localdelivery");
 
+        // Convert String -> socketAddress
         SocketAddress inSocketAddress = parseSocketAddress(remote);
         Set<SocketAddress> outSocketAddressSet = Arrays.stream(destinations.split(",")).map(s -> parseSocketAddress(s)).collect(Collectors.toSet());
 
-	DatagramSocket inSocket = new DatagramSocket(inSocketAddress); 
+        // Create socket server (in) and player (out)
+	    DatagramSocket inSocket = new DatagramSocket(inSocketAddress); 
         DatagramSocket outSocket = new DatagramSocket();
+        
+        // Buffer
         byte[] buffer = new byte[4 * 1024];
        
         while (true) {
-          DatagramPacket inPacket = new DatagramPacket(buffer, buffer.length);
- 	  inSocket.receive(inPacket);  // if remote is unicast
+            // Prepare packet
+            DatagramPacket inPacket = new DatagramPacket(buffer, buffer.length);
+ 	        inSocket.receive(inPacket);  // if remote is unicast // and recived
 
-          System.out.print(".");
-          for (SocketAddress outSocketAddress : outSocketAddressSet) 
-            {
-              outSocket.send(new DatagramPacket(buffer, inPacket.getLength(), outSocketAddress));
+            // Debug "."
+            System.out.print(".");
+            for(SocketAddress outSocketAddress : outSocketAddressSet) 
+            {   
+                // The Data
+                byte[] data = Arrays.copyOf(inPacket.getData(), inPacket.getLength());
+                
+                // [0, 12] - IV; [12, until end] - ciphertext
+                byte[] iv = Arrays.copyOfRange(data, 0, 12);
+                byte[] cipherTxt = Arrays.copyOfRange(data, 12, data.length);
+
+                // Prepare Key
+                Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
+                GCMParameterSpec spec = new GCMParameterSpec(128, iv);
+                cipher.init(Cipher.DECRYPT_MODE, key, spec);
+
+                // ""Cypher"" to plain
+                byte[] plain = cipher.doFinal(cipherTxt);
+
+                // Sends to player
+                DatagramPacket outPacket = new DatagramPacket(plain, plain.length, outSocketAddress);
+                outSocket.send(outPacket);            
             }
         }
     }
 
+    // ---------------------------------------------------------------------------------------------------
+    // Function Auxliar to convert ("IP:Port" -> InetSocketAddress type)
     private static InetSocketAddress parseSocketAddress(String socketAddress) 
     {
         String[] split = socketAddress.split(":");
